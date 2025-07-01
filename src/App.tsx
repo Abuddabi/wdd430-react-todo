2780bdfe060c0c769d75779e8fc2772eb6352a9c
Egor Sotnikov <web.abudabi@gmail.com>
2025-06-30 22:42:16 -0600

    Simple layout

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,33 @@ function App() {
 
   return (
     <>
-      <div className="text-blue-500">Test</div>
+      <form>
+        <label>
+          <input
+            className="border border-gray-300 rounded-md p-2"
+            type="text"
+            id="item"
+          />
+        </label>
+        <button type="submit">Add</button>
+      </form>
+      <h1 className="text-2xl font-bold my-6">Todo List</h1>
+      <ul>
+        <li className="flex items-center justify-between mb-2">
+          <label>
+            <input className="mr-2" type="checkbox" />
+            Item 1
+          </label>
+          <button className="ml-4">Delete</button>
+        </li>
+        <li className="flex items-center justify-between mb-2">
+          <label>
+            <input className="mr-2" type="checkbox" />
+            Item 2
+          </label>
+          <button className="ml-4">Delete</button>
+        </li>
+      </ul>
     </>
   );
 }
